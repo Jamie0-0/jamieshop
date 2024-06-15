5c4a1d7e6697abe62948ae41b3ef64cd6c5d9995
Jamie Chang <kght1256@gmail.com>
2024-06-15 16:37:35 +0800

    專案建立

---
+++ src/main/java/com/example/jamieshop/jamieshop_workspace/JamieshopWorkspaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.jamieshop.jamieshop_workspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamieshopWorkspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamieshopWorkspaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jamieshop/jamieshop_workspace/JamieshopWorkspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jamieshop.jamieshop_workspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamieshopWorkspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
